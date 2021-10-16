380a04fc1a738e31a60aaeda1521c72526d949fb
Inxighto <85790454+Inxighto@users.noreply.github.com>
2021-10-16 18:24:53 +0600

    spawner(update)
    add SpawnManager;

---
+++ Assets/Main/Scripts/Core/Spawner/SpawnerManager.cs
@@ -0,0 +1,35 @@
+using Main.Scripts.Core;
+using Main.Scripts.Enemy.Data;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class SpawnerManager : MonoBehaviour
+{
+    [FoldoutGroup("Scriptable Object")][SerializeField] private BaseEnemySO _baseEnemySO;
+    private Transform _target;
+    [SerializeField] private bool _spawnAtOnce = false;
+    [FoldoutGroup("Settings")][HideIf(nameof(_spawnAtOnce))][SerializeField] private float _startTime = 1;
+    [FoldoutGroup("Settings")][HideIf(nameof(_spawnAtOnce))][SerializeField] private float _repeatTime = 3;
+
+    private void Start()
+    {
+        _target = GameObject.FindGameObjectWithTag(TagConstants.Player).transform;
+        if (_spawnAtOnce == false)
+        {
+            InvokeRepeating(nameof(Spawn), _startTime, _repeatTime);
+        }
+        else
+        {
+            Spawn();
+        }
+    }
+
+    private void Spawn()
+    {
+        var enemy = Instantiate(_baseEnemySO.EnemyPrefab, transform.position, Quaternion.identity);
+        enemy.Setup(new EnemyData
+        {
+            BaseEnemySO = _baseEnemySO, Target = _target
+        });
+    }
+}
