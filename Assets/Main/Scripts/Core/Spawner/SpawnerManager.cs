d0de572540a892259d22e37f80d645fc664dbdbb
Silentdesu <silentdesu@gmail.com>
2021-10-16 21:14:43 +0600

    Refactored
    Refactored SpawnerManager
    Refactored BaseEnemy

--- Assets/Main/Scripts/Core/Spawner/SpawnerManager.cs
+++ Assets/Main/Scripts/Core/Spawner/SpawnerManager.cs
@@ -5,23 +5,23 @@ using UnityEngine;
 
 public class SpawnerManager : MonoBehaviour
 {
-    [FoldoutGroup("Scriptable Object")][SerializeField] private BaseEnemySO _baseEnemySO;
+    [FoldoutGroup("Scriptable Object"), SerializeField] private BaseEnemySO _baseEnemySO;
     private Transform _target;
-    [SerializeField] private bool _spawnAtOnce = false;
-    [FoldoutGroup("Settings")][HideIf(nameof(_spawnAtOnce))][SerializeField] private float _startTime = 1;
-    [FoldoutGroup("Settings")][HideIf(nameof(_spawnAtOnce))][SerializeField] private float _repeatTime = 3;
+    [SerializeField] private bool _repeatingSpawn = false;
+    [FoldoutGroup("Settings"), HideIf(nameof(_repeatingSpawn)), SerializeField] private float _startTime = 1;
+    [FoldoutGroup("Settings"), HideIf(nameof(_repeatingSpawn)), SerializeField] private float _repeatTime = 3;
 
     private void Start()
     {
         _target = GameObject.FindGameObjectWithTag(TagConstants.Player).transform;
-        if (_spawnAtOnce == false)
+        
+        if (_repeatingSpawn == false)
         {
             InvokeRepeating(nameof(Spawn), _startTime, _repeatTime);
+            return;
         }
-        else
-        {
-            Spawn();
-        }
+
+        Spawn();
     }
 
     private void Spawn()
@@ -29,7 +29,8 @@ public class SpawnerManager : MonoBehaviour
         var enemy = Instantiate(_baseEnemySO.EnemyPrefab, transform.position, Quaternion.identity);
         enemy.Setup(new EnemyData
         {
-            BaseEnemySO = _baseEnemySO, Target = _target
+            BaseEnemySO = _baseEnemySO, 
+            Target = _target
         });
     }
 }
