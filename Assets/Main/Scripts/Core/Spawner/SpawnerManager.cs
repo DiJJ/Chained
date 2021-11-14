3de5d87c15809b0d2a35f40e7d134e005ac2f07e
Silentdesu <silentdesu@gmail.com>
2021-11-14 18:09:14 +0600

    Change SpawnManager
    Remade from InvokeRepeating() to StartCouroutine()

--- Assets/Main/Scripts/Core/Spawner/SpawnerManager.cs
+++ Assets/Main/Scripts/Core/Spawner/SpawnerManager.cs
@@ -1,5 +1,7 @@
+using System.Collections;
+using Main.ScriptablesObjects;
 using Main.Scripts.Core;
-using Main.Scripts.Enemy.Data;
+using Main.Scripts.Enemy;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
@@ -7,22 +9,32 @@ public class SpawnerManager : MonoBehaviour
 {
     [FoldoutGroup("Scriptable Object")][SerializeField] private BaseEnemySO _baseEnemySO;
     private Transform _target;
-    [SerializeField] private bool _spawnAtOnce = false;
-    [FoldoutGroup("Settings")][HideIf(nameof(_spawnAtOnce))][SerializeField] private float _startTime = 1;
-    [FoldoutGroup("Settings")][HideIf(nameof(_spawnAtOnce))][SerializeField] private float _repeatTime = 3;
-    [FoldoutGroup("Settings")] [HideIf(nameof(_spawnAtOnce))] [SerializeField] private float _rangeMin = 1;
-    [FoldoutGroup("Settings")] [HideIf(nameof(_spawnAtOnce))] [SerializeField] private float _rangeMax = 10;
+    
+    [SerializeField] private bool _repeatSpawn = false;
+    [FoldoutGroup("Settings")] [ShowIf(nameof(_repeatSpawn))] [SerializeField] private float _delayTime = 1;
+    [FoldoutGroup("Settings")] [SerializeField] private float _rangeMin = 1;
+    [FoldoutGroup("Settings")] [SerializeField] private float _rangeMax = 10;
 
     private void Start()
     {
         _target = GameObject.FindGameObjectWithTag(TagConstants.Player).transform;
-        if (_spawnAtOnce == false)
+        
+        if (_repeatSpawn)
         {
-            InvokeRepeating(nameof(Spawn), _startTime, _repeatTime);
+            StartCoroutine(SpawnRoutine());
+            return;
         }
-        else
+        
+        Spawn();
+    }
+
+    private IEnumerator SpawnRoutine()
+    {
+        while (_repeatSpawn)
         {
             Spawn();
+
+            yield return new WaitForSeconds(_delayTime);
         }
     }
 
@@ -34,5 +46,10 @@ public class SpawnerManager : MonoBehaviour
         {
             BaseEnemySO = _baseEnemySO, Target = _target
         });
+    }
+
+    private void OnDisable()
+    {
+        StopAllCoroutines();
     }
 }
